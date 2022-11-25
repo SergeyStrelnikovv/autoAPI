4139d76bddac9912a1c094fce22c4df4a9744384
sergey <sso96@mail.ru>
2022-11-25 10:25:32 +0300

    initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,19 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+      given()
+              .baseUri("http://localhost:9999/api/v1")
+              .when()
+              .get("/demo/accounts")
+              .then()
+              .statusCode(200)
+              .header("Content-Type", "application/json; charset=UTF-8");
+
+    }
+}
